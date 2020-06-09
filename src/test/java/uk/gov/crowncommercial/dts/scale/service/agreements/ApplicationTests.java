725612818f574fc69849877cf848cf5567a10805
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-09 07:08:51 +0100

    SFAT-7 - temp removed empty test

--- src/test/java/uk/gov/crowncommercial/dts/scale/service/agreements/ApplicationTests.java
+++ src/test/java/uk/gov/crowncommercial/dts/scale/service/agreements/ApplicationTests.java
@@ -1,12 +1,9 @@
 package uk.gov.crowncommercial.dts.scale.service.agreements;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class ApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
