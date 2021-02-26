94393cd3f5769ea6d05b952e57485ceb9a54da11
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-02-26 11:12:09 +0000

    SINF-361 - fixed assertNull import

--- src/test/java/uk/gov/crowncommercial/dts/scale/service/agreements/converter/RouteToMarketConverterTest.java
+++ src/test/java/uk/gov/crowncommercial/dts/scale/service/agreements/converter/RouteToMarketConverterTest.java
@@ -1,7 +1,7 @@
 package uk.gov.crowncommercial.dts.scale.service.agreements.converter;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 import uk.gov.crowncommercial.dts.scale.service.agreements.model.dto.BuyingMethod;
 import uk.gov.crowncommercial.dts.scale.service.agreements.model.dto.RouteToMarketDTO;
