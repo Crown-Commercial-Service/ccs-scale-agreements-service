a497e97aa89a8671073fb190de6250d2123225c5
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-05-21 10:38:22 +0100

    AS-51 - tidied comment

--- src/main/java/uk/gov/crowncommercial/dts/scale/service/agreements/converter/TimestampConverter.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/service/agreements/converter/TimestampConverter.java
@@ -4,10 +4,9 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
-import uk.gov.crowncommercial.dts.scale.service.agreements.model.dto.SupplierStatus;
 
 /**
- * Converts a DB organisation status to a {@link SupplierStatus}
+ * Converts a DB Timestamp to an Instant.
  */
 @Component
 public class TimestampConverter extends AbstractConverter<Timestamp, Instant> {
