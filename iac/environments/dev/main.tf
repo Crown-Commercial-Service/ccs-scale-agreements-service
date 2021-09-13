4f35b06f0b09b2bf31d738bd2080200b61b7e510
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-13 14:32:07 +0100

    SCC-605 - fixed space names (#29)

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -5,7 +5,7 @@
 #########################################################
 module "deploy-all" {
   source      = "../../modules/configs/deploy-all"
-  space       = "develop"
+  space       = "development"
   environment = "dev"
   cf_username = var.cf_username
   cf_password = var.cf_password
