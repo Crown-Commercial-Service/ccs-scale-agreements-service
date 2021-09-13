cd1c124465d0ac0903d86bcb7f3f0604c3aa7074
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-09-13 16:29:24 +0100

    SCC-605 - renamed int space (#30)
    * SCC-605 - renamed int space

--- iac/environments/int/main.tf
+++ iac/environments/int/main.tf
@@ -5,7 +5,7 @@
 #########################################################
 module "deploy-all" {
   source      = "../../modules/configs/deploy-all"
-  space       = "INT"
+  space       = "int"
   environment = "int"
   cf_username = var.cf_username
   cf_password = var.cf_password
