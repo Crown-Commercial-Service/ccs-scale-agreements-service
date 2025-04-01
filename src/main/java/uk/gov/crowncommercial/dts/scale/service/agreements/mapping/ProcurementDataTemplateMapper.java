9374b5f881b6be2352dafbd4b6dfedc9ea8b9922
NToppingCCS <nathan.topping@crowncommercial.gov.uk>
2025-04-01 13:08:56 +0100

    Mapping from JSON to an object needs to be explicit following the upgrade to Boot 3

--- src/main/java/uk/gov/crowncommercial/dts/scale/service/agreements/mapping/ProcurementDataTemplateMapper.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/service/agreements/mapping/ProcurementDataTemplateMapper.java
@@ -1,7 +1,10 @@
 package uk.gov.crowncommercial.dts.scale.service.agreements.mapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import uk.gov.crowncommercial.dts.scale.service.agreements.model.dto.ProcurementDataTemplate;
 import uk.gov.crowncommercial.dts.scale.service.agreements.model.entity.ProcurementQuestionTemplate;
 
@@ -10,6 +13,20 @@ import uk.gov.crowncommercial.dts.scale.service.agreements.model.entity.Procurem
  */
 @Mapper(componentModel = "spring")
 public interface ProcurementDataTemplateMapper {
-    @Mapping(source = "templatePayload", target = "criteria")
+    @Mapping(source = "templatePayload", target = "criteria", qualifiedByName = "jsonToObject")
     ProcurementDataTemplate procurementQuestionTemplateToProcurementDataTemplate(ProcurementQuestionTemplate dbModel);
+
+    @Named("jsonToObject")
+    static Object jsonToObject(Object sourceJson) throws JsonProcessingException {
+        // We just want to deserialize the JSON string we have into an object
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            return mapper.readValue(sourceJson.toString(), Object.class);
+        } catch (Exception ex) {
+            // Fail silently
+        }
+
+        return null;
+    }
 }
